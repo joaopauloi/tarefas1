1f34a846c1040d633a26e899926f4b00a5d3d68e
joaopauloi <joopauloneppel@hotmail.com>
2023-08-14 10:53:51 -0300

    Tarefas 1

---
+++ tarefas.py
@@ -0,0 +1,13 @@
+nome = "João Paulo"
+altura = 1,86
+idade = 2023-2005
+peso = 62
+
+print (nome)
+print (idade , "anos")
+print (altura , "de altura")
+print (peso ,"kg")
+if idade>17:
+    print ("Maior de idade") 
+else:
+    print("Menor de idade")

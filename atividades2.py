9072cb2df644f11a69777916aff204ed3ca63a76
João Paulo <140860849+joaopauloi@users.noreply.github.com>
2023-09-21 13:56:40 -0300

    Update and rename atividades2 to atividades2.py

---
+++ atividades2.py
@@ -0,0 +1,11 @@
+
+peso = float(input("Digite seu peso em quilogramas: "))
+altura = float(input("Digite sua altura em metros: "))
+
+imc = peso / (altura ** 2)
+
+print(f"Seu IMC é: {imc:.2f}")
+
+
+
+
